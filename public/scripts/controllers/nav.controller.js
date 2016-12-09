8c64590b40fd103d649d4743bda50094d062c568
Steve Larsen <stevelarsen01@gmail.com>
2016-12-09 10:50:10 -0600

    format header, footer, and background image

--- public/scripts/controllers/nav.controller.js
+++ public/scripts/controllers/nav.controller.js
@@ -1,15 +1,19 @@
 app.controller("NavController", ["$http", "AuthFactory", function($http, AuthFactory) {
     console.log('NavController started');
     var self = this;
-    self.trips = [];
     var authFactory = AuthFactory;
+    self.trips = [];
+
+    self.status = {
+        isLoggedIn: false
+    };
 
     // Function to Login
     self.logIn = function() {
         authFactory.logIn().then(function(currentUser) {
-          console.log('lc current user', currentUser);
-          authFactory.idToken = currentUser.idToken;
-        }) ;
+            console.log('lc current user', currentUser);
+            authFactory.idToken = currentUser.idToken;
+        });
     }; // End Login
 
     // Function to Logout
