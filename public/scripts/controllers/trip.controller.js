ba09284762848535484bc67d4277faa6704b1867
Steve Larsen <stevelarsen01@gmail.com>
2016-12-13 14:41:52 -0600

    trips accordion experiment

--- public/scripts/controllers/trip.controller.js
+++ public/scripts/controllers/trip.controller.js
@@ -1,6 +1,10 @@
 app.controller("TripController", ['$http','$filter', '$q', function($http, $filter, $q) {
 console.log('TripController started');
 var self = this;
+self.status = {};
+self.status.isOuterGroup1Open = true;
+self.status.isOuterGroup2Open = true;
+console.log(self.status);
 
   self.users = [
     {id: 1, name: 'awesome user1', status: 2, group: 4, groupName: 'admin'},
@@ -122,121 +126,121 @@ var self = this;
 
 
 
-// self.items = [
-//                     {
-//                         name: "item1",
-//                         desc: "Item 1",
-//                         subitems: [
-//                             {
-//                                 name: "subitem1",
-//                                 desc: "Sub-Item 1",
-//                                 subitems: [
-//                                     {
-//                                         name: "subitem1",
-//                                         desc: "Sub-Item 1"
-//                                     },
-//                                     {
-//                                         name: "subitem2",
-//                                         desc: "Sub-Item 2"
-//                                     },
-//                                     {
-//                                         name: "subitem2",
-//                                         desc: "Sub-Item 2"
-//                                     }]
-//                             },
-//                             {
-//                                 name: "subitem2",
-//                                 desc: "Sub-Item 2",
-//                                 subitems: [
-//                                     {
-//                                         name: "subitem1",
-//                                         desc: "Sub-Item 1"
-//                                     },
-//                                     {
-//                                         name: "subitem2",
-//                                         desc: "Sub-Item 2"
-//                                     },
-//                                     {
-//                                         name: "subitem2",
-//                                         desc: "Sub-Item 2"
-//                                     }]
-//
-//                             },
-//                             {
-//                                 name: "subitem2",
-//                                 desc: "Sub-Item 2",
-//                                 subitems: [
-//                                     {
-//                                         name: "subitem1",
-//                                         desc: "Sub-Item 1"
-//                                     },
-//                                     {
-//                                         name: "subitem2",
-//                                         desc: "Sub-Item 2"
-//                                     },
-//                                     {
-//                                         name: "subitem2",
-//                                         desc: "Sub-Item 2"
-//                                     }]
-//
-//                             }]
-//                     },
-//                     {
-//                         name: "item2",
-//                         desc: "Item 2",
-//                         subitems: [
-//                             {
-//                                 name: "subitem1",
-//                                 desc: "Sub-Item 1"
-//                             },
-//                             {
-//                                 name: "subitem2",
-//                                 desc: "Sub-Item 2"
-//                             },
-//                             {
-//                                 name: "subitem2",
-//                                 desc: "Sub-Item 2"
-//                             }]
-//                     },
-//                     {
-//                         name: "item3",
-//                         desc: "Item 3",
-//                         subitems: [
-//                             {
-//                                 name: "subitem1",
-//                                 desc: "Sub-Item 1"
-//                             },
-//                             {
-//                                 name: "subitem2",
-//                                 desc: "Sub-Item 2"
-//                             },
-//                             {
-//                                 name: "subitem2",
-//                                 desc: "Sub-Item 2"
-//                             }]
-//                     }
-//                 ];
-//
-// self.default = self.items[2];
-//
-// self.oneAtATime = true;
-//
-// self.groups = [{
-//     title: 'Dynamic Group Header - 1',
-//     content: 'Dynamic Group Body - 1'
-// }, {
-//     title: 'Dynamic Group Header - 2',
-//     content: 'Dynamic Group Body - 2'
-// }];
-//
-// self.items = ['Item 1', 'Item 2', 'Item 3'];
-//
-// self.addItem = function() {
-//     var newItemNo = self.items.length + 1;
-//     self.items.push('Item ' + newItemNo);
-// };
-//
-//
-//
+self.items = [
+                    {
+                        name: "item1",
+                        desc: "Item 1",
+                        subitems: [
+                            {
+                                name: "subitem1",
+                                desc: "Sub-Item 1",
+                                subitems: [
+                                    {
+                                        name: "subitem1",
+                                        desc: "Sub-Item 1"
+                                    },
+                                    {
+                                        name: "subitem2",
+                                        desc: "Sub-Item 2"
+                                    },
+                                    {
+                                        name: "subitem2",
+                                        desc: "Sub-Item 2"
+                                    }]
+                            },
+                            {
+                                name: "subitem2",
+                                desc: "Sub-Item 2",
+                                subitems: [
+                                    {
+                                        name: "subitem1",
+                                        desc: "Sub-Item 1"
+                                    },
+                                    {
+                                        name: "subitem2",
+                                        desc: "Sub-Item 2"
+                                    },
+                                    {
+                                        name: "subitem2",
+                                        desc: "Sub-Item 2"
+                                    }]
+
+                            },
+                            {
+                                name: "subitem2",
+                                desc: "Sub-Item 2",
+                                subitems: [
+                                    {
+                                        name: "subitem1",
+                                        desc: "Sub-Item 1"
+                                    },
+                                    {
+                                        name: "subitem2",
+                                        desc: "Sub-Item 2"
+                                    },
+                                    {
+                                        name: "subitem2",
+                                        desc: "Sub-Item 2"
+                                    }]
+
+                            }]
+                    },
+                    {
+                        name: "item2",
+                        desc: "Item 2",
+                        subitems: [
+                            {
+                                name: "subitem1",
+                                desc: "Sub-Item 1"
+                            },
+                            {
+                                name: "subitem2",
+                                desc: "Sub-Item 2"
+                            },
+                            {
+                                name: "subitem2",
+                                desc: "Sub-Item 2"
+                            }]
+                    },
+                    {
+                        name: "item3",
+                        desc: "Item 3",
+                        subitems: [
+                            {
+                                name: "subitem1",
+                                desc: "Sub-Item 1"
+                            },
+                            {
+                                name: "subitem2",
+                                desc: "Sub-Item 2"
+                            },
+                            {
+                                name: "subitem2",
+                                desc: "Sub-Item 2"
+                            }]
+                    }
+                ];
+
+self.default = self.items[2];
+
+self.oneAtATime = true;
+
+self.groups = [{
+    title: 'Dynamic Group Header - 1',
+    content: 'Dynamic Group Body - 1'
+}, {
+    title: 'Dynamic Group Header - 2',
+    content: 'Dynamic Group Body - 2'
+}];
+
+self.items = ['Item 1', 'Item 2', 'Item 3'];
+
+self.addItem = function() {
+    var newItemNo = self.items.length + 1;
+    self.items.push('Item ' + newItemNo);
+};
+
+
+
 
 }]); // END: TripController
