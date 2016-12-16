306440935df119f34d613142ceb4d85de2ec1be1
Steve Larsen <stevelarsen01@gmail.com>
2016-12-15 19:57:41 -0600

    format home page

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -1,5 +1,54 @@
-app.controller("HomeController", [function() {
-    console.log('HomeController started');
+app.controller("HomeController", [function($scope) {
     var self = this;
+
+    self.myInterval = 3000;
+    self.noWrapSlides = false;
+    self.active = 0;
+
+    self.slides = [{
+        image: "../../assets/images/DSC00294.jpg"
+    }, {
+        image: "../../assets/images/DSC00421.jpg"
+    }, {
+        image: "../../assets/images/DSC00428.jpg"
+    }, {
+        image: "../../assets/images/DSC00576.jpg"
+    }, {
+        image: "../../assets/images/DSC00863.jpg"
+    }, {
+        image: "../../assets/images/DSC02365.jpg"
+    }, {
+        image: "../../assets/images/DSC01324.jpg"
+    }, {
+        image: "../../assets/images/DSC02459.jpg"
+    }, {
+        image: "../../assets/images/DSC02810.jpg"
+    }, {
+        image: "../../assets/images/DSC02643.jpg"
+    }, {
+        image: "../../assets/images/DSC02665.jpg"
+    }, {
+        image: "../../assets/images/DSC01981.jpg"
+    }, {
+        image: "../../assets/images/DSC02024.jpg"
+    }, {
+        image: "../../assets/images/DSC03034.jpg"
+    }, {
+        image: "../../assets/images/DSC03507.jpg"
+    }, {
+        image: "../../assets/images/DSC03633.jpg"
+    }, {
+        image: "../../assets/images/DSC04139.jpg"
+    }, {
+        image: "../../assets/images/DSC04160.jpg"
+    }, {
+        image: "../../assets/images/DSC04565.jpg"
+    }, {
+        image: "../../assets/images/DSC04573.jpg"
+    }, {
+        image: "../../assets/images/DSC02139.jpg"
+    }, {
+        image: "../../assets/images/DSC00576.jpg"
+    }];
 
 }]); // END: HomeController
