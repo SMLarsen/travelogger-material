a8c757ecf4d291326aa5dd5971719d3d0b6947d1
Steve Larsen <stevelarsen01@gmail.com>
2016-12-23 10:45:54 -0600

    geocoding and markers working in test area

--- public/scripts/controllers/map.controller.js
+++ public/scripts/controllers/map.controller.js
@@ -1,24 +1,40 @@
-app.controller("MapController", ['MapFactory', '$http', '$filter', '$routeParams', 'NgMap', function(MapFactory, $http, $filter, $routeParams, NgMap) {
+app.controller("MapController", ['$http', '$filter', '$routeParams', 'NgMap', 'GeoCoder', function($http, $filter, $routeParams, NgMap, GeoCoder) {
     console.log('MapController started');
     var self = this;
-    var mapFactory = MapFactory;
 
     self.newAddress = '';
     self.location = {};
     self.lat = '';
     self.lng = '';
+    self.locationArray = [];
+    self.startLocation = '';
+    self.endLocation = '';
+
+    function buildLocationArray() {
+        var newLocation = {
+            pos: [self.lat, self.lng],
+            name: self.newAddress
+        };
+        self.locationArray.push(newLocation);
+        console.log(self.locationArray);
+        self.startLocation = self.locationArray[0];
+        self.endLocation = self.locationArray[self.locationArray.length - 1];
+        console.log('start:', self.startLocation);
+        console.log('stop:', self.endLocation);
+    }
 
     self.findAddress = function() {
-        var geocoder = new google.maps.Geocoder();
-        geocoder.geocode( { "address": self.newAddress }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-                self.location = results[0].geometry.location;
-                self.lat      = self.location.lat();
-                self.lng      = self.location.lng();
-              console.log("Latitude: " + self.lat);
-              console.log("Longitude: " + self.lng);
-            }
+
+        GeoCoder.geocode({
+            address: self.newAddress
+        }).then(function(result) {
+            self.location = result[0].geometry.location;
+            self.lat = self.location.lat();
+            self.lng = self.location.lng();
+            console.log("Latitude: " + self.lat);
+            console.log("Longitude: " + self.lng);
+            buildLocationArray();
         });
-      };
+    };
 
 }]); // END: DayController
