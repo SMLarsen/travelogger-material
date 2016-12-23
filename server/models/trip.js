2763fc5e9559abc63d8578d6be409d84b64878ac
Steve Larsen <stevelarsen01@gmail.com>
2016-12-23 12:45:52 -0600

    reformat trip add with pin buttons

--- server/models/trip.js
+++ server/models/trip.js
@@ -10,17 +10,29 @@ var tripSchema = new Schema({
         type: String,
         required: true
     },
+    destination: {
+        type: String
+    },
+    destination_location: {
+        type: Object
+    },
     begin_date: {
         type: Date
     },
     begin_location: {
         type: String
     },
+    begin_map_location: {
+        type: Object
+    },
     end_date: {
         type: Date
     },
     end_location: {
         type: String
+    },
+    end_map_location: {
+        type: Object
     },
     travellers: {
         type: String
