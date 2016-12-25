6ab27327b13eeaa3e7463769f610a13d45f72c83
Steve Larsen <stevelarsen01@gmail.com>
2016-12-25 07:52:19 -0600

    delete trip on mytrips

--- public/scripts/controllers/mytrip.controller.js
+++ public/scripts/controllers/mytrip.controller.js
@@ -47,5 +47,29 @@ app.controller('MyTripController', ['MyTripFactory', '$http', 'AuthFactory', 'Ng
                 }
             );
     }; // End addTrip
+    // Function to delete a trip
+    self.deleteTrip = function(tripID) {
+      console.log('tripID:', tripID);
+        myTripFactory.deleteTrip(tripID)
+            .then(function(response) {
+                    myTripFactory.deleteTripDays(tripID)
+                        .then(function(response) {
+                                myTripFactory.getTrips()
+                                    .then(function(response) {
+                                            self.trips = response;
+                                            console.log('Trip deleted', self.trips);
+                                        },
+                                        function(err) {
+                                            console.log('Error getting trips after delete', err);
+                                        });
+                            },
+                            function(err) {
+                                console.log('Error deleting trip', err);
+                            });
+                },
+                function(err) {
+                    console.log('Unable to delete trip', err);
+                });
+    }; // End deleteTrip
 
 }]); // END: MyTripController
